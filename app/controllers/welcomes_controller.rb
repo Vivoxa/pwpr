402bb18c79faa42b0505d08060236092e1f8b02c
Lorand Sandor <lorand.sandor@sage.com>
2016-10-02 14:47:09 +0100

    Remove welcome controller, test and view

--- app/controllers/welcomes_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-class WelcomesController < ApplicationController
-  # efore_action :authenticate_user!
-
-  # GET /welcomes
-  # GET /welcomes.json
-  def index
-  end
-end

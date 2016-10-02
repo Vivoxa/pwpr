0d2275500954d5577f638ac5fd72c19940b55e9e
Lorand Sandor <lorand.sandor@sage.com>
2016-10-02 13:21:58 +0100

    Sort migrations out after royce changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   royce_roles %w[admin scheme_owner scheme_user_r scheme_user_rw scheme_user_rwe]
-  after_initialize :set_default_role, :if => :new_record?
 
-  def set_default_role
-    self.role ||= :user
-  end
 end

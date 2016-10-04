a177b80d9340c1d631e33509875e196bc2f17d11
nige-s <nigel.surtees@sage.com>
2016-10-04 23:49:37 +0100

    add more specs

--- spec/models/scheme_spec.rb
+++ spec/models/scheme_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Scheme, type: :model do
 
+  before do
+    subject.email = 'nigelsurtees@wvivoxa.com'
+    subject.password = 'khgsdfgaskgfdkag'
+    subject.save
+  end
+
   context 'Roles' do
     it 'expects the correct roles to be available' do
-      expect(Scheme.available_role_names).to eq %w(owner admin user_r user_rw)
+    expect(Scheme.available_role_names).to eq %w(owner admin user_r user_rw)
     end
 
     it 'expects owner to be an available role' do
@@ -26,5 +32,27 @@ RSpec.describe Scheme, type: :model do
     it 'expects name to be an attribute' do
       expect(subject.respond_to?(:name)).to be true
     end
+
+    context 'when assigning a role' do
+      it 'expects the scheme to have that role' do
+        subject.add_role :owner
+        expect(subject.has_role? :owner).to be true
+        expect(subject.owner?).to be true
+        subject.admin!
+        expect(subject.admin?).to be true
+        expect(subject.has_role? :admin).to be true
+
+      end
+    end
+
+    context 'when removing a role' do
+      it 'expects the scheme to NOT have that role' do
+        subject.add_role :owner
+        expect(subject.has_role? :owner).to be true
+        subject.remove_role :owner
+        expect(subject.has_role? :owner).to be false
+      end
+    end
+
   end
 end

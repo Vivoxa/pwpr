aa3e65408c53a0012bb9bbadf197a92f2226f861
nige-s <nigel.surtees@sage.com>
2016-10-04 17:20:21 +0100

    remove user model and associated views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # resources :welcomes, only: :index

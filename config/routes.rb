c0a7de4bec0635c667ad85ef207a9b0af18da7fa
nige-s <nigel.surtees@sage.com>
2016-10-04 19:56:16 +0100

    remove user resource from routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # resources :welcomes, only: :index
-  resources :users
 
   # You can have the root of your site routed with "root"
   root 'visitors#index'

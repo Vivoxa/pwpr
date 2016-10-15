fc071e0b18797092e72f296b3953d9882bbd0657
Lorand Sandor <lorand.sandor93@gmail.com>
2016-10-15 18:01:11 +0100

    Add controller logic for showing and updating permissions

--- app/controllers/company_operator/company_operator_permissions_controller.rb
+++ app/controllers/company_operator/company_operator_permissions_controller.rb
@@ -3,8 +3,26 @@ class CompanyOperatorPermissionsController < ApplicationController
   authorize_resource class: CompanyOperator
 
   def show
+    @user = CompanyOperator.find_by_id(params[:id])
+    @available_roles = user.available_roles - CompanyOperator::PERMISSIONS
+    @available_permissions = user.available_roles - CompanyOperator::ROLES
   end
 
-  def update_roles_and_permissions
+  def update
+    @user = CompanyOperator.find_by_id(params[:id])
+
+    begin
+      @user.add_role params[:role]
+
+      permissions = params[:permissions] # This should be and array/hash of selected permissions
+
+      permissions.each do |p|
+        @user.add_role p
+      end
+    rescue => e
+      redirect_to company_operator_show_path, error: "An error occured! User #{@user.email}'s permissions were not updated.", status: :unprocessable_entity # 422
+    end
+
+    redirect_to company_operator_show_path @user.id, notice: 'Permissions updated succesfully!', status: :ok # 200 if
   end
 end
--- app/controllers/scheme_operator/scheme_operator_permissions_controller.rb
+++ app/controllers/scheme_operator/scheme_operator_permissions_controller.rb
@@ -3,8 +3,26 @@ class SchemeOperatorPermissionsController < ApplicationController
   authorize_resource class: SchemeOperator
 
   def show
+    @user = SchemeOperator.find_by_id(params[:id])
+    @available_roles = user.available_roles - SchemeOperator::PERMISSIONS
+    @available_permissions = user.available_roles - SchemeOperator::ROLES
   end
 
-  def update_roles_and_permissions
+  def update
+    @user = SchemeOperator.find_by_id(params[:id])
+
+    begin
+      @user.add_role params[:role]
+
+      permissions = params[:permissions] # This should be and array/hash of selected permissions
+
+      permissions.each do |p|
+        @user.add_role p
+      end
+    rescue => e
+      redirect_to scheme_operator_show_path, error: "An error occured! User #{@user.email}'s permissions were not updated.", status: :unprocessable_entity # 422
+    end
+
+    redirect_to scheme_operator_show_path @user.id, notice: 'Permissions updated succesfully!', status: :ok # 200 if
   end
 end

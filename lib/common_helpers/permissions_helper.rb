bf13e88102ac17c9f01b2d797a90f8bc4c9c524d
Lorand Sandor <lorand.sandor93@gmail.com>
2016-10-22 16:23:41 +0100

    Update redirect path for interfaced permissions helper method

--- lib/common_helpers/permissions_helper.rb
+++ lib/common_helpers/permissions_helper.rb
@@ -2,7 +2,7 @@ module CommonHelpers
   module PermissionsHelper
     protected
 
-    def modify_roles_and_permissions
+    def modify_roles_and_permissions(resource_path)
       current = @user.role_list
       begin
         remove_unselected_permissions!
@@ -13,11 +13,11 @@ module CommonHelpers
       rescue
         roll_back_roles!(current)
 
-        redirect_to scheme_operator_path @user.id, error: "An error occured! User #{@user.email}'s permissions were not updated.", status: :unprocessable_entity # 422
+        redirect_to resource_path, error: "An error occured! User #{@user.email}'s permissions were not updated."
         return
       end
 
-      redirect_to scheme_operator_path @user.id, notice: 'Permissions updated succesfully!', status: :ok # 302
+      redirect_to resource_path, notice: 'Permissions updated succesfully!'
     end
 
     private

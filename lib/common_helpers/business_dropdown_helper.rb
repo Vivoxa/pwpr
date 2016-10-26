2abd30383015f9d10333fda1716b0cbdef8b6d85
nige-s <nigelsurtees@hotmail.co.uk>
2016-10-26 19:07:46 +0100

    improve specs for business dropdown helper

--- lib/common_helpers/business_dropdown_helper.rb
+++ lib/common_helpers/business_dropdown_helper.rb
@@ -2,11 +2,8 @@ module CommonHelpers
   module BusinessDropdownHelper
     def update_businesses
       schemes = Scheme.where('id = ?', params[:scheme_id])
-      @businesses = if schemes.any?
-                      schemes.first.businesses
-                    else
-                      []
-                    end
+      @businesses = []
+      @businesses = schemes.first.businesses if schemes.any?
 
       respond_to do |format|
         format.js

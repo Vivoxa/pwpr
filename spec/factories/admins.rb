28864d57c9907a68d2dff7851c639f11cc0fffb4
Lorand Sandor <lorand.sandor93@gmail.com>
2016-10-27 19:31:48 +0100

    Update factories to match new roles

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -4,12 +4,12 @@ FactoryGirl.define do
     name 'Marti Mcfly'
     password 'my_password'
 
-    factory :admin_full_access do
+    factory :admin_super_admin do
       email 'doc.brown@back_to_future.com'
       name 'Doc Brown'
       password 'my_password'
       after(:create) do |admin|
-        admin.add_role 'full_access'
+        admin.add_role 'super_admin'
       end
     end
   end
--- spec/factories/company_operators.rb
+++ spec/factories/company_operators.rb
@@ -29,39 +29,48 @@ FactoryGirl.define do
       end
     end
 
-    factory :company_operator_with_contact do |co_c|
-      co_c.after :build do |instance|
+    factory :company_operator_with_co_super_user do |co_s|
+      co_s.after :build do |instance|
         instance.business_id = FactoryGirl.create(:business).id
       end
       after(:create) do |co|
-        co.add_role 'co_contact'
+        co.add_role 'co_super_user'
       end
     end
 
-    factory :company_operator_with_co_user_r do |co_r|
+    factory :company_operator_with_co_users_r do |co_r|
       co_r.after :build do |instance|
         instance.business_id = FactoryGirl.create(:business).id
       end
       after(:create) do |co|
-        co.add_role 'co_user_r'
+        co.add_role 'co_users_r'
       end
     end
 
-    factory :company_operator_with_co_user_rw do |co_rw|
-      co_rw.after :build do |instance|
+    factory :company_operator_with_co_users_w do |co_w|
+      co_w.after :build do |instance|
         instance.business_id = FactoryGirl.create(:business).id
       end
       after(:create) do |co|
-        co.add_role 'co_user_rw'
+        co.add_role 'co_users_w'
       end
     end
 
-    factory :company_operator_with_co_user_rwe do |co_rwe|
-      co_rwe.after :build do |instance|
+    factory :company_operator_with_co_users_e do |co_e|
+      co_e.after :build do |instance|
         instance.business_id = FactoryGirl.create(:business).id
       end
       after(:create) do |co|
-        co.add_role 'co_user_rwe'
+        co.add_role 'co_users_e'
+      end
+    end
+
+    factory :company_operator_with_co_users_d do |co_d|
+      co_d.after :build do |instance|
+        instance.business_id = FactoryGirl.create(:business).id
+      end
+      after(:create) do |co|
+        co.add_role 'co_users_d'
       end
     end
   end

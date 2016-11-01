86d9f464181db614d6dcbe40f0f1e12c76dba9ad
Lorand Sandor <lorand.sandor93@gmail.com>
2016-11-01 19:48:25 +0000

    Add spec for permission definitions (all user types)

---
+++ spec/lib/permissions_for_role/shared_definitions_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe PermissionsForRole::SharedDefinitions do
+  context 'Constants' do
+    it 'sets SHARED_PERMISSIONS' do
+      expect(subject.class::SHARED_PERMISSIONS).to eq %w(sc_users_r sc_users_w sc_users_e sc_users_d
+                                                        co_users_r co_users_w co_users_d co_users_e
+                                                        businesses_r businesses_w businesses_d businesses_e
+                                                        schemes_r schemes_w schemes_d schemes_e).freeze
+    end
+  end
+end

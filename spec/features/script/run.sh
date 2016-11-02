0caa75aedf3442d7fbf25e9255775244eca259d7
nige-s <nigelsurtees@hotmail.co.uk>
2016-11-02 13:57:24 +0000

    add chrome to automation

--- spec/features/script/run.sh
+++ spec/features/script/run.sh
@@ -5,5 +5,5 @@ spec_output="--format html --out /desktop/results/$(date +"%F")/TEST_RUN_$(date 
 
 docker-compose build
 
-SELENIUM_BROWSER=${VARIABLE:-firefox} # set default as firefox
-docker-compose run -e BROWSER=$SELENIUM_BROWSER feature_tests  && echo 'test suite complete.'
+SELENIUM_BROWSER=${VARIABLE:-chrome} # set default as firefox
+docker-compose run -e BROWSER=$SELENIUM_BROWSER feature_tests $* $spec_output/results.html && echo 'test suite complete.'

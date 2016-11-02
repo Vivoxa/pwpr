fe761099cc7bc072f109e2d1db5598b9c14a6c9f
nige-s <nigelsurtees@hotmail.co.uk>
2016-11-02 17:04:21 +0000

    add switch to save automation results to desktop

--- spec/features/script/run.sh
+++ spec/features/script/run.sh
@@ -6,4 +6,13 @@ spec_output="--format html --out /desktop/results/$(date +"%F")/TEST_RUN_$(date 
 docker-compose build
 
 SELENIUM_BROWSER=${VARIABLE:-chrome} # set default as firefox
-docker-compose run -e BROWSER=$SELENIUM_BROWSER feature_tests $* $spec_output/results.html && echo 'test suite complete.'
+
+if [[  $@ == *"--save"*  ]] ;then
+  echo 'Output results to /desktop/results...'
+
+  docker-compose run -e BROWSER=$SELENIUM_BROWSER feature_tests $spec_output/results.html && echo 'test suite complete.'
+else
+  docker-compose run -e BROWSER=$SELENIUM_BROWSER feature_tests && echo 'test suite complete.'
+fi
+
+

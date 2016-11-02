ea26c0c7cfddd3b8dc16e8980a6544a5fcb6b22c
nige-s <nigelsurtees@hotmail.co.uk>
2016-11-02 17:08:24 +0000

    rubocop changes

--- spec/features/spec/support/capybara_helper.rb
+++ spec/features/spec/support/capybara_helper.rb
@@ -8,23 +8,24 @@ Capybara.default_driver = :selenium
 Capybara.default_max_wait_time = 15
 Capybara.always_include_port = true
 
-  Capybara.configure do |config|
-    config.register_driver :selenium_hub do |app|
-      browser = ENV.fetch('BROWSER').downcase
-      puts "Using browser: #{browser}"
-      capabilities = Selenium::WebDriver::Remote::Capabilities.public_send(browser.to_sym)
-      Capybara::Selenium::Driver.new(
-        app,
-        browser: :remote,
-        url: ENV.fetch('SELENIUM_HUB_URL'),
-        desired_capabilities: capabilities)
-    end
-    config.default_driver = :selenium_hub
-    config.javascript_driver = :selenium_hub
+Capybara.configure do |config|
+  config.register_driver :selenium_hub do |app|
+    browser = ENV.fetch('BROWSER').downcase
+    puts "Using browser: #{browser}"
+    capabilities = Selenium::WebDriver::Remote::Capabilities.public_send(browser.to_sym)
+    Capybara::Selenium::Driver.new(
+      app,
+      browser:              :remote,
+      url:                  ENV.fetch('SELENIUM_HUB_URL'),
+      desired_capabilities: capabilities
+    )
+  end
+  config.default_driver = :selenium_hub
+  config.javascript_driver = :selenium_hub
 
-    Capybara::Screenshot.register_driver(:selenium_hub) do |driver, path|
-      driver.browser.save_screenshot(path)
-    end
+  Capybara::Screenshot.register_driver(:selenium_hub) do |driver, path|
+    driver.browser.save_screenshot(path)
+  end
 
   RSpec.configure do |config|
     config.include Capybara::DSL
@@ -32,7 +33,7 @@ Capybara.always_include_port = true
     config.around(:all) do |example|
       @_session = Capybara::Session.new(:selenium_hub)
       example.run
-#      @_session.driver.browser.quit
+      #      @_session.driver.browser.quit
     end
   end
 end

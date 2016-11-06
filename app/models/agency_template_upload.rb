932a7e45740ad7151670629466e6e9e85718025e
nige-s <nigelsurtees@hotmail.co.uk>
2016-11-06 22:47:04 +0000

    add dropdown for year, validation to model and improve specs

--- app/models/agency_template_upload.rb
+++ app/models/agency_template_upload.rb
@@ -1,12 +1,16 @@
 class AgencyTemplateUpload < ActiveRecord::Base
   belongs_to :scheme
 
+  VALID_YEARS_FOR_UPLOAD = [2010, 2011, 2012, 2013, 2014, 2015].freeze
+
   validates_presence_of :scheme_id
   validates_presence_of :year
   validates_presence_of :uploaded_at
   validates_presence_of :uploaded_by_id
   validates_presence_of :uploaded_by_type
   validates_presence_of :filename
+
+  validates_inclusion_of :year, in: VALID_YEARS_FOR_UPLOAD
 
   def initialize(attributes = {})
     super

ada3e887a979f4d6e9afc303909f816bac2c993f
Lorand Sandor <lorand.sandor93@gmail.com>
2016-11-12 16:44:45 +0000

    Refactor specs

--- spec/features/spec/company_operators/sign_in_spec.rb
+++ spec/features/spec/company_operators/sign_in_spec.rb
@@ -11,20 +11,18 @@ RSpec.describe 'CompanyOperator', js: true do
     it_behaves_like 'a button', '/company_operators/sign_in', 'Log in'
 
     context 'signing out' do
-      let(:sign_out_btn) { nil }
+      let(:sign_out_btn) { find_by_id('signing_btn') }
       before do
         sign_in('CompanyOperator', 'co_director_0@pwpr.com', 'min700si')
-        @sign_out_btn = find_by_id('signing_btn')
       end
 
       it 'expects to find a button for sign out' do
-        expect(@sign_out_btn).not_to be_nil
+        expect(sign_out_btn).not_to be_nil
       end
 
       it 'expects the sign out link to be correct' do
-        expect(@sign_out_btn['href']).to include('/company_operators/sign_out')
+        expect(sign_out_btn['href']).to include('/company_operators/sign_out')
       end
     end
-
   end
 end
--- spec/features/spec/helper_methods.rb
+++ spec/features/spec/helper_methods.rb
@@ -1,12 +1,11 @@
 def sign_in(user_type, email, password)
-
   case user_type
-    when 'Admin'
-      visit '/admins/sign_in'
-    when 'SchemeOperator'
-      sign_in_btn_id = 'sc_login'
-    when 'CompanyOperator'
-      sign_in_btn_id = 'co_login'
+  when 'Admin'
+    visit '/admins/sign_in'
+  when 'SchemeOperator'
+    sign_in_btn_id = 'sc_login'
+  when 'CompanyOperator'
+    sign_in_btn_id = 'co_login'
   end
 
   if sign_in_btn_id

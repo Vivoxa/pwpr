702203afdf87c00b3fa510c96507dc07cbe40945
nige-s <nigelsurtees@hotmail.co.uk>
2016-11-12 07:57:47 +0000

    wip restrict access to scheme actions if not active

--- lib/permissions_for_role/role/admin_ability.rb
+++ lib/permissions_for_role/role/admin_ability.rb
@@ -16,6 +16,14 @@ module PermissionsForRole
 
         can %i(read new create update destroy), Admins::RegistrationsController if user.super_admin?
       end
+
+
+      def scheme_permissions(user)
+        can :read, Scheme, id: user.scheme_ids if user.schemes_r?
+        can %i(new create), Scheme if user.schemes_w?
+        can %i(edit update), Scheme, id: user.scheme_ids if user.schemes_e?
+        can :destroy, Scheme, id: user.scheme_ids if user.schemes_d?
+      end
     end
   end
 end

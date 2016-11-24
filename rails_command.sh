0a408ba22de7de16011d6811a35c980feb334767
nige-personal <nigelsurtees@hotmail.co.uk>
2016-11-24 21:33:53 +0000

    ed

--- rails_command.sh
+++ rails_command.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$1" = 'production' ]; then
-  "RAILS_ENV=production bundle exec rake assets:precompile && RAILS_ENV=production bundle && RAILS_ENV=production bundle exec rake db:migrate && RAILS_ENV=production bundle exec rails s -p 3000 -b '0.0.0.0'"
+  RAILS_ENV=production bundle exec rake assets:precompile && RAILS_ENV=production bundle && RAILS_ENV=production bundle exec rake db:migrate && RAILS_ENV=production bundle exec rails s -p 3000 -b '0.0.0.0'
 else
-  "RAILS_ENV=preprod bundle && RAILS_ENV=preprod bundle exec rake db:drop db:create db:migrate db:seed && RAILS_ENV=preprod bundle exec rails s -p 3000 -b '0.0.0.0'"
+  RAILS_ENV=preprod bundle && RAILS_ENV=preprod bundle exec rake db:drop db:create db:migrate db:seed && RAILS_ENV=preprod bundle exec rails s -p 3000 -b '0.0.0.0'
 fi

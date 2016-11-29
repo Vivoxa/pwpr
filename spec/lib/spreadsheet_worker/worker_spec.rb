a1c6998f2c16ef37d5fdb8cb4ec96eea0e4e1416
Lorand Sandor <lorand.sandor93@gmail.com>
2016-11-29 21:29:35 +0000

    Add worker specs

---
+++ spec/lib/spreadsheet_worker/worker_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe SpreadsheetWorker::Worker do
+  let(:bunny) { Bunny.new(hostname: 'queue_rabbitmq:5672', automatically_recover: false, log_file: 'log/spreadsheet_worker.log', log_level: :info) }
+  let(:channel) { double(Bunny::Channel) }
+  let(:queue) { double(Bunny::Queue) }
+  let(:event) { 'event message' }
+  let(:queue_name) { 'test_queue_name' }
+
+  context 'when starting the worker' do
+    before :each do
+      allow(Bunny).to receive(:new).and_return bunny
+      allow(bunny).to receive(:start).and_return true
+      allow(bunny).to receive(:create_channel).and_return channel
+      allow(channel).to receive(:queue).and_return queue
+      allow(queue).to receive(:name).and_return queue_name
+      allow(queue).to receive(:subscribe)
+      allow(channel).to receive(:prefetch)
+    end
+
+    after :each do
+      subject.start
+    end
+
+    it 'calls prefetch' do
+      expect(subject).to receive(:prefetch).once
+    end
+
+    it 'subscribed with the correct params' do
+      expect(queue).to receive(:subscribe).with(manual_ack: true, block: true)
+    end
+  end
+end

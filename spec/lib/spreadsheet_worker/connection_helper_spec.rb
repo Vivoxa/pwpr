f7e23cdbe524e354309198f849593137221c4c43
Lorand Sandor <lorand.sandor93@gmail.com>
2016-11-29 21:32:31 +0000

    Remove connection_helper_spec

--- spec/lib/spreadsheet_worker/connection_helper_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe SpreadsheetWorker::ConnectionHelper do
-  context 'Constants' do
-    it 'sets SHARED_PERMISSIONS' do
-      expect(subject.class::QUEUE_NAME).to eq 'spreadsheet_processing_queue'
-    end
-  end
-end

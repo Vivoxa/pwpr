7eb15eefee5441d0066821beb08f6564c36551ff
Lorand Sandor <lorand.sandor93@gmail.com>
2016-12-10 17:15:44 +0000

    Create base handler

--- lib/spreadsheet_worker/sheet_processor/registrations_handler.rb
+++ lib/spreadsheet_worker/sheet_processor/registrations_handler.rb
@@ -1,23 +1,21 @@
-require 'roo-xls'
-require_relative '../sheet_map_loader/map'
-
 module SpreadsheetWorker
   module SheetProcessor
-    class RegistrationsHandler
-      include SheetMapLoader
+    class RegistrationsHandler < BaseHandler
+      def initialize
+        @registration = Registration.new
+        @business = get_business(row_array)
+      end
 
       def process(row)
         row_array = registrations.row(row)
-        @registration = Registration.new
 
         process_contact(row_array)
-        get_or_create_business(row_array)
         process_registered_address(row_array)
         process_correspondence_address(row_array)
         process_audit_address(row_array)
         process_small_producer(row_array)
         process_regular_producer(row_array)
-        process_other(row_array)
+        finalise_registration_processing(row_array)
       end
 
       private
@@ -67,14 +65,14 @@ module SpreadsheetWorker
       end
 
       def process_contact(row)
-        @contact = Contact.new
-        @contact.title = column_value(row, map['contact']['title']['field'])
-        @contact.first_name = column_value(row, map['contact']['first_name']['field'])
-        @contact.last_name = column_value(row, map['contact']['last_name']['field'])
-        @contact.email = column_value(row, map['contact']['email']['field'])
-        @contact.telephone_1 = column_value(row, map['contact']['phone']['field'])
-        @contact.fax = column_value(row, map['contact']['fax']['field'])
-        @contact
+        contact = Contact.new
+        contact.title = column_value(row, map['contact']['title']['field'])
+        contact.first_name = column_value(row, map['contact']['first_name']['field'])
+        contact.last_name = column_value(row, map['contact']['last_name']['field'])
+        contact.email = column_value(row, map['contact']['email']['field'])
+        contact.telephone_1 = column_value(row, map['contact']['phone']['field'])
+        contact.fax = column_value(row, map['contact']['fax']['field'])
+        contact.business = @business
       end
 
       def process_small_producer(row)
@@ -99,7 +97,7 @@ module SpreadsheetWorker
         # binding.pry
       end
 
-      def process_other(row)
+      def finalise_registration_processing(row)
         @registration.licensor = column_value(row, map['licensor']['field'])
         @registration.turnover = column_value(row, map['turnover']['field'])
         @registration.allocation_method_used = column_value(row, map['allocation']['method_used']['field'])
@@ -107,65 +105,13 @@ module SpreadsheetWorker
         @registration.change_detail = ChangeDetail.where(modification: column_value(row, map['change_to_member_application_or_obligation']['field'])).first
         @registration.resubmission_reason = ResubmissionReason.where(reason: column_value(row, map['resubmission_reason']['field'])).first
         @registration.packaging_sector_main_activity = PackagingSectorMainActivity.where(type: column_value(row, map['change_to_member_application_or_obligation']['field'])).first
+        @registration.business = @business
         @registration.sic_code = @registration.business.sic_code
         # binding.pry
       end
 
-      def get_or_create_business(row)
-        npwd = column_value(row, map['npwd']['field'])
-        business = Business.where(NPWD: npwd).first if npwd
-
-        unless business
-          business = Business.new
-          business.trading_name = column_value(row, map['company_name']['field'])
-          business.company_number = column_value(row, map['company_house_no']['field'])
-          business.NPWD = npwd
-          business.scheme = Scheme.first
-          business.country_of_business_registration = CountryOfBusinessRegistration.where(country: column_value(row, map['registered']['country']['field'])).first
-          business.sic_code = SicCode.where(code: column_value(row, map['sic_code']['field'])).first
-          business.scheme_ref = column_value(row, map['scheme_ref']['field'])
-          business.business_type = BusinessType.where(name: column_value(row, map['company_type']['field'])).first
-          business.business_subtype = BusinessSubtype.where(name: column_value(row, map['company_subtype']['field'])).first
-          business.year_first_reg = Date.today.year
-          business.year_last_reg = Date.today.year
-        end
-
-        @registration.business = business
-        @contact.business = business
-        # binding.pry
-      end
-
-      def map
-        map_loader.load(:registrations)
-      end
-
-      def map_loader
-        @map ||= SpreadsheetWorker::SheetMapLoader::Map.new
-      end
-
       def registrations
         spreadsheet.sheet(2)
-      end
-
-      def spreadsheet
-        @sheet ||= Roo::Spreadsheet.open('./public/template_sheet.xls')
-      end
-
-      def column_value(row, letter)
-        index = transform_to_index(letter)
-        value = row[index]
-
-        return boolean_column_value(value) if ['Y', 'N'].include? value
-        value
-      end
-
-      def boolean_column_value(value)
-        value == 'Y' ? true : false
-      end
-
-      def transform_to_index(letter)
-        set = ('A'..'EZ').to_a
-        set.find_index(letter) if set.include? letter
       end
     end
   end

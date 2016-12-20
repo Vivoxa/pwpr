32908cbc0179dcd1f1a9b2e1b12e7ce4a9bfec31
nige-personal <nigelsurtees@hotmail.co.uk>
2016-12-20 19:41:36 +0000

    rename app dir

--- script/setup.sh
+++ script/setup.sh
@@ -4,6 +4,6 @@ docker-compose rm
 echo build docker image
 command="RAILS_ENV=development bundle && RAILS_ENV=development bundle exec rake db:drop db:create db:setup && RAILS_ENV=development bundle exec rails s -p 3000 -b '0.0.0.0'"
 
-docker build --rm -f Dockerfile  --build-arg APP_DIR=pp_pwpr --build-arg  COMMAND="$command" -t pwpr_service .
+docker build --rm -f Dockerfile  --build-arg APP_DIR=pwpr --build-arg  COMMAND="$command" -t pwpr_service .
 
 echo setup complete

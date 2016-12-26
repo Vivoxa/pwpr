06084c7ffe193169208be6d5a0be0c73355d7d72
nige-personal <nigelsurtees@hotmail.co.uk>
2016-12-26 11:07:56 +0000

    add logging to SchemeMailer

--- app/mailers/scheme_mailer.rb
+++ app/mailers/scheme_mailer.rb
@@ -1,10 +1,14 @@
 class SchemeMailer < ApplicationMailer
   def registration_email(business, filename, file_path, year)
-    attachments[filename] = File.read(file_path)
-    @business = business
-    @scheme = business.scheme
-    @year = year
-    @url  = 'http://app-pwpr.com/sign_in'
-    mail(to: business.correspondence_contact.email, subject: "[RESPONSE REQUIRED]: Registration form #{year}")
+    begin
+      attachments[filename] = File.read(file_path)
+      @business = business
+      @scheme = business.scheme
+      @year = year
+      @url = 'http://app-pwpr.com/sign_in'
+      mail(to: business.correspondence_contact.email, subject: "[RESPONSE REQUIRED]: Registration form #{year}")
+    rescue => e
+      Rails.logger.warn("SchemeMailer::registration_email() ERROR: #{e.message}")
+    end
   end
 end

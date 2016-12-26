2cd8572b8c83b20e188dae2929923ceaf3c4ed88
nige-personal <nigelsurtees@hotmail.co.uk>
2016-12-26 09:36:35 +0000

    remove invalid attribute form business form and show page

--- script/build_image.sh
+++ script/build_image.sh
@@ -11,7 +11,7 @@ set -e
       echo ''
       echo '*****************************************************************************************************************************************************************************************************************'
       echo '          Building the command to run on the container'
-      command="bundle exec rake assets:precompile RAILS_ENV=production && bundle RAILS_ENV=production && bundle exec rake db:migrate RAILS_ENV=production && bundle exec rails s -p 3000 -b '0.0.0.0' RAILS_ENV=production"
+      command="RAILS_ENV=production bundle exec rake assets:precompile && RAILS_ENV=production bundle && RAILS_ENV=production bundle exec rake db:migrate && RAILS_ENV=production bundle exec rails s -p 3000 -b '0.0.0.0'"
       echo $command
       echo '*****************************************************************************************************************************************************************************************************************'
       echo ''

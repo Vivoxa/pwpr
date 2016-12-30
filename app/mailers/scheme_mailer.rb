1da180b7db5a6cae42f4392d26e02c381f38e09a
nige-personal <nigelsurtees@hotmail.co.uk>
2016-12-30 22:01:54 +0000

    merge

--- app/mailers/scheme_mailer.rb
+++ app/mailers/scheme_mailer.rb
@@ -4,9 +4,31 @@ class SchemeMailer < ApplicationMailer
     @business = business
     @scheme = business.scheme
     @year = year
-    @url = 'http://app-pwpr.com/sign_in'
-    mail(to: business.correspondence_contact.email, subject: "[RESPONSE REQUIRED]: Registration form #{year}")
+    @url = ENV['APP_CO_SIGN_IN_URL']
+
+    email_settings = LookupValues::Email::EmailSettings.for('registration_email')
+    subject = email_settings['subject'] % {year: year}
+
+    mail(to: recipient_email, subject: subject)
   rescue => e
     Rails.logger.warn("SchemeMailer::registration_email() ERROR: #{e.message}")
+  end
+
+  def scheme_director_info(businesses, scheme, year, recipient)
+    @scheme = scheme
+    @year = year
+    @url = ENV['APP_SO_SIGN_IN_URL']
+    @recipient = recipient
+
+    file_path = Pdf::RegistrationFormEmailedBusinesses.pdf(businesses, year)
+
+    email_settings = LookupValues::Email::EmailSettings.for('scheme_director_info')
+
+    subject = email_settings['subject'] % {year: year}
+    report_name = email_settings['report_name'] % {scheme_id: scheme.id, year: year}
+
+    attachments[report_name] = File.read(file_path)
+
+    mail(to: recipient.email, subject: subject)
   end
 end
--- lib/reporting/worker.rb
+++ lib/reporting/worker.rb
@@ -7,6 +7,11 @@ module Reporting
 
       event_data.retrieve_business_ids.each do |business_id|
         report_instance.process_report(business_id, event_data.year, current_user(event_data))
+
+        businesses = Business.where(id: business_ids)
+
+        logger.info('Emailing report to SchemeOperator')
+        email_scheme_operator(businesses, businesses.first.scheme, event_data.year, current_user(event_data))
       end
       @queue_manager.log(:info, " [x] Event '#{event}' has been processed!")
     end

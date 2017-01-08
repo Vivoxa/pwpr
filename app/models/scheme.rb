806ae6f40b445f349196ed5d6573d65536d446dc
nige-personal <nigelsurtees@hotmail.co.uk>
2017-01-08 20:18:44 +0000

    remove permissions for co and refactor from email address for reports

--- app/models/scheme.rb
+++ app/models/scheme.rb
@@ -7,4 +7,8 @@ class Scheme < ActiveRecord::Base
   has_and_belongs_to_many :scheme_operators
 
   validates_presence_of :scheme_country_code_id, :name
+
+  def email_friendly_name
+    "#{name.downcase.tr(' ', '_')}@#{LookupValues::Email::EmailSettings.for('domain_name')}"
+  end
 end

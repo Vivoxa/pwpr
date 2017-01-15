2c3121e740b7dee57668f717bb2cd02fe279226c
nige-personal <nigelsurtees@hotmail.co.uk>
2017-01-15 19:37:36 +0000

    add specs business controller

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -36,7 +36,7 @@ class BusinessesController < BaseController
   # PATCH/PUT /businesses/1
   # PATCH/PUT /businesses/1.json
   def update
-    update_object(@business, businesses_url, business_params)
+    update_object(@business, "#{businesses_url}?scheme_id=#{@business.scheme_id}", business_params)
   end
 
   # DELETE /businesses/1

b1d4cbc23bf616bd05ec997e8003cb3a01a5d061
Lorand Sandor <lorand.sandor93@gmail.com>
2017-01-27 01:27:56 +0000

    Update licensors spec

--- spec/lib/spreadsheet_worker/sheet_processor/sheet_handlers/licensors_handler_spec.rb
+++ spec/lib/spreadsheet_worker/sheet_processor/sheet_handlers/licensors_handler_spec.rb
@@ -4,20 +4,20 @@ RSpec.describe SpreadsheetWorker::SheetProcessor::SheetHandlers::LicensorsHandle
   subject { described_class.new(valid_agency_template.id) }
   let(:speadsheet) { double('RooSheet') }
   let(:valid_row_array) { ['NPWD100815',	'Camerons Brewery Ltd',	'NPWD100815',	'1',	'Angel Inn Thirsk',	'00000000',	'N/A',	'N/A',	'N/A',	'N/A',	'N/A',	'Richard',	'Forster',	'01429 852025',	'richardforster@cameronsbrewery.com',	'Long Street',	'Topcliffe',	'',	'Thirsk',	'YO7 3RW'] }
+  let(:empty_row) { [] }
   let(:valid_business) { Business.new(id: 1) }
-  let(:invalid_business) { Business.new }
   let(:valid_agency_template) { AgencyTemplateUpload.new(id: 1) }
-  let(:invalid_agency_template) { AgencyTemplateUpload.new }
   let(:server_file_path) { double('server/file/test.xls')}
   let(:filename) { 'test.xls' }
   let(:licensor) { subject.instance_variable_get(:@licensor) }
   let(:contact) { subject.instance_variable_get(:@contact) }
+  let(:registered_address) { Address.new(id: 3) }
+  let(:contact_address) { Address.new(id: 4) }
   let(:address_type) { AddressType.new(id: 1) }
 
   before do
     allow(InputOutput::ServerFileHandler).to receive(:server_file_path_for).and_return server_file_path
     allow(valid_agency_template).to receive(:filename).and_return filename
-    allow(invalid_agency_template).to receive(:filename).and_return filename
     allow(Roo::Spreadsheet).to receive(:open).and_return speadsheet
     allow(AddressType).to receive(:where).and_return [address_type]
     allow(valid_business.addresses).to receive(:where).and_return []
@@ -26,6 +26,14 @@ RSpec.describe SpreadsheetWorker::SheetProcessor::SheetHandlers::LicensorsHandle
     allow_any_instance_of(Address).to receive(:save!).and_return true
     allow(Address).to receive(:where).and_return []
     allow_any_instance_of(Licensor).to receive(:save!).and_return true
+    allow(AddressType).to receive(:where).with(title: 'Correspondence').and_return [address_type]
+    allow(AddressType).to receive(:id_from_setting).with('Correspondence').and_return 1
+    allow(AddressType).to receive(:where).with(title: 'Audit').and_return [address_type]
+    allow(AddressType).to receive(:id_from_setting).with('Audit').and_return 2
+    allow(AddressType).to receive(:where).with(title: 'Registered').and_return [address_type]
+    allow(AddressType).to receive(:id_from_setting).with('Registered').and_return 3
+    allow(AddressType).to receive(:where).with(title: 'Contact').and_return [address_type]
+    allow(AddressType).to receive(:id_from_setting).with('Contact').and_return 4
   end
 
   describe '#process' do
@@ -35,6 +43,65 @@ RSpec.describe SpreadsheetWorker::SheetProcessor::SheetHandlers::LicensorsHandle
         allow(AgencyTemplateUpload).to receive(:find_by_id).and_return valid_agency_template
         allow(speadsheet).to receive_message_chain(:sheet, :drop).and_return [valid_row_array]
         subject.process
+      end
+
+      context 'when empty row' do
+        before do
+          allow(speadsheet).to receive_message_chain(:sheet, :drop).and_return [empty_row]
+        end
+
+        it 'skips the row' do
+          expect(Registration).not_to receive(:new)
+        end
+      end
+
+      context 'when address exists' do
+        before do
+          allow(valid_business.addresses).to receive(:where).and_return [1]
+        end
+
+        it 'return early' do
+          expect(Address).not_to receive(:new)
+        end
+      end
+
+      context 'creates registered address' do
+        before do
+          allow(Address).to receive(:new).and_return registered_address
+          subject.process
+        end
+
+        it 'has correct address_line_1' do
+          expect(registered_address.address_line_1).to eq 'Long Street'
+        end
+
+        it 'has correct address_line_2' do
+          expect(registered_address.address_line_2).to eq 'Topcliffe'
+        end
+
+        it 'has correct address_line_3' do
+          expect(registered_address.address_line_3).to eq ''
+        end
+
+        it 'has correct town' do
+          expect(registered_address.town).to eq 'Thirsk'
+        end
+
+        it 'has correct post_code' do
+          expect(registered_address.post_code).to eq 'YO7 3RW'
+        end
+
+        it 'has correct address_type' do
+          expect(registered_address.address_type).to eq address_type
+        end
+
+        it 'has correct business' do
+          expect(registered_address.business).to eq valid_business
+        end
+
+        it 'object is valid' do
+          expect(registered_address).to be_valid
+        end
       end
 
       context 'licensor' do
@@ -79,20 +146,6 @@ RSpec.describe SpreadsheetWorker::SheetProcessor::SheetHandlers::LicensorsHandle
         it 'creates a valid object' do
           expect(contact).to be_valid
         end
-      end
-    end
-
-    context 'when object fields are invalid' do
-      before do
-        allow(Business).to receive(:where).and_return [invalid_business]
-        allow(invalid_business.addresses).to receive(:where).and_return []
-        allow(AgencyTemplateUpload).to receive(:find_by_id).and_return invalid_agency_template
-        allow(speadsheet).to receive_message_chain(:sheet, :drop).and_return [valid_row_array]
-        subject.process
-      end
-
-      it 'creates a invalid object' do
-        expect(licensor).to be_invalid
       end
     end
   end

ac16c29ffeb336d3210ca7495bdd2ed6d4ac4dbb
Lorand Sandor <lorand.sandor93@gmail.com>
2017-01-30 19:07:50 +0000

    Remove membership id from businesses through migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161225080451) do
+ActiveRecord::Schema.define(version: 20170130190602) do
 
   create_table "address_types", force: :cascade do |t|
     t.string   "title",       limit: 255, null: false

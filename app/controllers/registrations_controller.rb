f7a2cc08c7ebd526e6db0a7b86d66ced1eeab0a1
Lorand Sandor <lorand.sandor93@gmail.com>
2017-02-05 03:00:09 +0000

    Finish controller logic for forms

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,13 +3,20 @@ class RegistrationsController < ApplicationController
   def new
     @business = Business.where(id: params[:business_id]).first
     @registration = Registration.new
-    error_redirect(@business, 'No Registration Details where found for this business!') and return if @business.registrations.empty?
-    #
-    # @producer = @business.registrations.last.regular_producer_details.last
   end
 
   # POST business/:id/registrations
   def create
+    @business = Business.where(id: params[:business_id]).first
+
+    error_redirect(@business, 'Invalid Sic Code!') and return unless formated_params[:sic_code]
+    error_redirect(@business, 'Packaging Sector no provided!') and return unless formated_params[:packaging_sector_main_activity]
+
+    registration = Registration.new(formated_params)
+    registration.business = @business
+    registration.save!
+
+    redirect_to @business, notice: "Registration Details for #{@business.name} were successfully saved!"
   end
 
   # GET business/:id/registrations/1/edit
@@ -21,5 +28,31 @@ class RegistrationsController < ApplicationController
 
   # PATCH/PUT business/:id/registrations/1
   def update
+    @business = Business.where(id: params[:business_id]).first
+    registration = @business.registrations.last
+
+    error_redirect(@business, 'Invalid Sic Code!') and return unless formated_params[:sic_code]
+    error_redirect(@business, 'Packaging Sector no provided!') and return unless formated_params[:packaging_sector_main_activity]
+
+    registration.attributes = formated_params
+    registration.save!
+
+    redirect_to @business, notice: "Registration Details for #{@business.name} were successfully updated!"
+  end
+
+  def registration_params
+    params.require(:registration).permit(:sic_code, :packaging_sector_main_activity, :submission_type,
+                                          :resubmission_reason, :turnover, :allocation_method_used)
+  end
+
+  private
+
+  def formated_params
+    reg_params = registration_params
+    reg_params[:sic_code] = SicCode.where(code: reg_params[:sic_code]).first
+    reg_params[:packaging_sector_main_activity] = PackagingSectorMainActivity.where(material: reg_params[:packaging_sector_main_activity]).first
+    reg_params[:submission_type] = SubmissionType.where(code: reg_params[:submission_type]).first
+    reg_params[:resubmission_reason] = ResubmissionReason.where(reason: reg_params[:resubmission_reason]).first
+    reg_params
   end
 end

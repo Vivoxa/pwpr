fd1daed61ec75175bb0d7759296d3344ae3dfab8
Lorand Sandor <lorand.sandor93@gmail.com>
2017-02-12 19:57:16 +0000

    add default to alloc method used

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211213113) do
+ActiveRecord::Schema.define(version: 20170211213112) do
 
   create_table "address_types", force: :cascade do |t|
     t.string   "title",       limit: 255, null: false
@@ -368,7 +368,7 @@ ActiveRecord::Schema.define(version: 20170211213113) do
     t.integer  "business_id",                       limit: 4,                                          null: false
     t.decimal  "turnover",                                    precision: 10, scale: 2,                 null: false
     t.boolean  "licensor",                                                             default: false
-    t.boolean  "allocation_method_used"
+    t.boolean  "allocation_method_used",                                               default: false, null: false
     t.datetime "created_at",                                                                           null: false
     t.datetime "updated_at",                                                                           null: false
   end
